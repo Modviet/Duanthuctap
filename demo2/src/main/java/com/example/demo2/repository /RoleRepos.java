f27a122391d5829382a2f837fb82ade7d22167f6
h4sin <105507345+h4sin@users.noreply.github.com>
2024-06-21 15:30:28 +0700

    Delete demo2/src/main/java/com/example/demo2/repository /RoleRepos.java

--- demo2/src/main/java/com/example/demo2/repository /RoleRepos.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo2.repository;
-
-import com.example.demo2.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepos extends JpaRepository<Role,Integer> {
-}
